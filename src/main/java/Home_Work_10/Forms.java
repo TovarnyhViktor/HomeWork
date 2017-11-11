7fc24dc0608e16ae98f607631d32c8c9fe0aa92d
TovarnyhViktor <milaslavska2@gmail.com>
2017-11-11 18:50:20 +0200

    Home Work 10 Выполнил пункт 1

--- src/main/java/Home_Work_10/Forms.java
+++ src/main/java/Home_Work_10/Forms.java
@@ -3,13 +3,11 @@ package Home_Work_10;
 import java.util.ArrayList;
 
 public class Forms {
-    public String name;
     public ArrayList<Form> forms = new ArrayList<>();
 
     @Override
     public String toString() {
-        return "Forms{" +
-                "name='" + name + '\'' +
+        return "Forms{" + '\'' +
                 ", forms=" + forms +
                 '}';
     }
--- src/main/java/Home_Work_10/Main.java
+++ src/main/java/Home_Work_10/Main.java
@@ -1,27 +1,47 @@
 package Home_Work_10;
-
 import com.alibaba.fastjson.JSON;
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static String datemain = "10/11/2017" ;
     public static void main(String[] args) throws FileNotFoundException {
-    Forms forms = new Forms();
-    forms.forms.add(new Form(Fruit.apple,10,"04/11/2017",5));
-    forms.forms.add(new Form(Fruit.banana,20,"04/11/2017",10));
-    String json = JSON.toJSONString(forms);
-        System.out.println(json);
-        readforms();
-
+    //save();
+    load();
+        SimpleDateFormat format = new SimpleDateFormat ("dd-MM-yyyy");
+        String datemainn = args.length == 0 ? datemain : args[0];
     }
-    static void readforms() throws FileNotFoundException {
+
+
+    static void load() throws FileNotFoundException {
         String json = new Scanner(new File("files/forms.json")).useDelimiter("\\Z").next();
         Forms forms = JSON.parseObject(json, Forms.class);
-        System.out.println(json);
         System.out.println(forms);
         System.out.println();
     }
+    static void save(){
+        Forms forms = new Forms();
+        forms.forms.add(new Form(Fruit.apple,10,"04/11/2017",5));
+        forms.forms.add(new Form(Fruit.banana,20,"04/11/2017",10));
+        forms.forms.add(new Form(Fruit.fig,15,"07/11/2017",45));
+        String json = JSON.toJSONString(forms);
+        try(FileWriter writer = new FileWriter("files\\forms.json", false)){
+            writer.write(json);
+            writer.flush();
+        }catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    /*List<Fruit> getSpoiledFruits(){
+
+    }*/
 }
